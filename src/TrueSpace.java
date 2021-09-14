fb9cc11b58ac1a5d2b3cdb4f28352ee6cb7d63e9
Samantha Owusu-Antwi <samantha.owusuantwi@duke.edu>
2021-09-14 18:38:28 -0400

    pass TrueSpace.java

--- src/TrueSpace.java
+++ src/TrueSpace.java
@@ -1,7 +1,7 @@
 public class TrueSpace {
     public long /*static void*/ calculateSpace(int[] sizes, int clusterSize) {
         // initialize int variable count
-        long count = 0;
+        long space = 0;
 
         // store each array in new array of doubles
         double[] sizes_d = new double[sizes.length];
@@ -9,32 +9,31 @@ public class TrueSpace {
             sizes_d[i] = sizes[i];
         }
 
+        // init cluster int var
+        long clusters = 0;
+
         // loop through each element in array
         for(double d : sizes_d){
-            // divide the element by clusterSize int and store result
-            double clusters = d / clusterSize;
-
-            // if the element % clusterSize != 0 add one to the stored result
-            if(d % clusterSize != 0){
-                clusters += 1;
+            if(d != 0) {
+                // divide the element by cluster size, always round it up, and add to clusters
+                clusters += Math.ceil(d / clusterSize); // looked up Math.ciel in documentation, was already aware of Math.round
             }
-
-            // Round the stored result using Math.round()
-            clusters = Math.round(clusters);
-
-            // multiply the clusterSize by the stored result and add it to the initialized variable count
-            count = (long) (count + (clusters * clusterSize));       // must return long not double (clusters * clusterSize leads to double) so cast to double
         }
 
-        return count;
-        //System.out.println(count);
+        // multiply the clusterSize by clusters and return
+        return (long) (clusters * clusterSize);       // must return long not double (clusters * clusterSize leads to double) so cast to double
+
+        //System.out.println((long) (clusters * clusterSize));
+        //System.out.println(clusters);
+        //System.out.println((clusters * clusterSize));
 
     }
 
     /*public static void main(String[] args) {
-        int[] sizes_ = {16,32,128,128,0};
-        int clusterSize_ = 32768;
+        int[] sizes_ = {1000000000, 1000000000, 1000000000, 1000000000, 1000000000, 1000000000, 1000000000, 1000000000, 1000000000, 1000000000, 1000000000, 1000000000, 1000000000, 1000000000, 1000000000, 1000000000, 1000000000, 1000000000, 1000000000, 1000000000, 1000000000, 1000000000, 1000000000, 1000000000, 1000000000, 1000000000, 1000000000, 1000000000, 1000000000, 1000000000, 1000000000, 1000000000, 1000000000, 1000000000, 1000000000, 1000000000, 1000000000, 1000000000, 1000000000, 1000000000, 1000000000, 1000000000, 1000000000, 1000000000, 1000000000, 1000000000, 1000000000, 1000000000, 1000000000, 1000000000};
+        int clusterSize_ = 1048576;
 
         calculateSpace(sizes_, clusterSize_);
+        // NOTE: using int for very large number sin java leads to weird mistakes in arithmetic
     }*/
 }
